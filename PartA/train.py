ac111845ade40855fd1edf50490196bce776e0ff
Aayushman <da24s016@smail.iitm.ac.in>
2025-04-18 17:54:24 +0000

    update

--- PartA/train.py
+++ PartA/train.py
@@ -6,6 +6,9 @@ import torch
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, random_split
 import yaml
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 # Define directories
 with open("config.yaml", "r") as f:
@@ -71,59 +74,8 @@ print(f"Validation half 2 size: {dataset_sizes['test']}")
 print(f"Class names: {class_names}")
 print(f"Using device: {device}")
 
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-class FlexibleCNN(nn.Module):
-    def __init__(self,
-                 input_channels=3,
-                 num_classes=10,
-                 num_conv_layers=5,
-                 conv_filters=32,
-                 filter_size=3,
-                 activation_func=nn.ReLU,
-                 dense_neurons=512,
-                 dropout_rate=0.25):
-
-        super().__init__()
-
-        self.conv_blocks = nn.ModuleList()
-        in_channels = input_channels
-
-        # Adding convolution-activation-batchnorm-maxpool blocks
-        for _ in range(num_conv_layers):
-            self.conv_blocks.append(nn.Sequential(
-                nn.Conv2d(in_channels=in_channels, out_channels=conv_filters,
-                          kernel_size=filter_size, stride=1, padding=filter_size//2),
-                nn.BatchNorm2d(conv_filters),  # Adding Batch Normalization
-                activation_func(),
-                nn.MaxPool2d(kernel_size=2)
-                # nn.Dropout2d(0.4)
-            ))
-            in_channels = conv_filters
-
-        # Flatten layer
-        self.flatten = nn.Flatten()
-
-        # Calculating input size for the first fully connected layer
-        input_size = conv_filters * (224 // (2**num_conv_layers))**2  # Calculate input size dynamically
-
-        # Fully connected layers with Dropout
-        self.fc1 = nn.Linear(input_size, dense_neurons)  # Use calculated input size
-        self.dropout = nn.Dropout(dropout_rate)  # Adding Dropout layer
-        self.fc2 = nn.Linear(dense_neurons, num_classes)
-
-    def forward(self, x):
-        for block in self.conv_blocks:
-            x = block(x)
-        x = self.flatten(x)
-        x = F.relu(self.fc1(x))
-        x = self.dropout(x)  # Apply Dropout during training
-        x = self.fc2(x)
-        return x
-
 # Example instantiation
+from model import FlexibleCNN
 model = FlexibleCNN(input_channels=3, num_classes=10, num_conv_layers=5,
                     conv_filters=48, filter_size=5, activation_func=nn.ReLU,
                     dense_neurons=512)
